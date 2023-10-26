ff75625a936ad60d29236688093ad184aac1d261
fxy <xyfeng0854@qq.com>
2023-10-26 11:17:40 +0800

    add then pull: will merge abort auto

--- a.js
+++ a.js
@@ -7,3 +7,9 @@ b233
 c3333
 番茄你个tomato
 console.log('1026-先add然后拉取会不会显示冲突呢?');
+console.log('1026-add之后但是还没commit的时候，拉取代码，会自动abort：');
+// error: Your local changes to the following files would be overwritten by merge:
+//     a.js
+// Please commit your changes or stash them before you merge.
+// Aborting
+
