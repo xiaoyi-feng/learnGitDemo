873c5c6c4e3c2184aa8dba3b4939dc87b89b45f6
fxy <xyfeng0854@qq.com>
2023-10-28 17:55:01 +0800

    record the no conflict merge

--- a.js
+++ a.js
@@ -23,4 +23,8 @@ llllllllllll-commit one
 555555555-one
 5555555-two
 //-------------10.28-----------------
-console.log('pull代码自动合并的时候，没有冲突的情况下，远程代码自动合并到本地文件，此时该文件的状态是modified？')
+console.log('pull代码自动合并的时候，没有冲突的情况下，远程代码自动合并到本地文件，合并之后远程改动的那些文件是否需要add？')
+// 不需要，pull = fetch + merge，本地代码与远程代码均改动有新的提交记录，本地代码的修改已add， commit
+// 此时pull, 本地的修改与远程修改执行合并，生成一个merge commit，
+// git status : nothing to commit, working tree clean.use "git push" to publish your local commits
+//只需要执行 git pull就行了
