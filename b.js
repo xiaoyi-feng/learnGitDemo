fd6ba51a1d8e9134c9b50dae138ad278f69797c7
fxy <xyfeng0854@qq.com>
2023-10-30 22:39:31 +0800

    1030|modified the b.js file

--- b.js
+++ b.js
@@ -3,3 +3,6 @@
 // this is modified by remote
 // create a new branch : bugFix
 // this is added by remote on bugFix分支
+// ======10/30==========
+//本地所做的修改：b.js which is on the feature branch on local
+// pull - > push，则会出现两个提交节点：本地的提交；远程代码与本地修改合并过程中自动生成的merge commit节点
