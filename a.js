441b0f8ff9406098bf928db280e1467ffdceecfa
fxy <xyfeng0854@qq.com>
2023-11-02 22:01:21 +0800

    112 | new local branch :112fxy

--- a.js
+++ a.js
@@ -34,3 +34,5 @@ console.log('pull代码自动合并的时候，没有冲突的情况下，远程
 // =========10|31===============
 // rebase 变基
 // git pull --rebase = git fetch + git rebase------local
+// ==============11/2==============
+// 在feature分支上切出一个新的本地分支112fxy, 并将其关联远程跟踪分支origin/feature
