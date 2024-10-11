459339fd1574222f1b925eef8a608badd3a9b70b
fxy <xyfeng0854@qq.com>
2024-10-11 10:24:08 +0800

    test stash commond

--- b.js
+++ b.js
@@ -2,9 +2,19 @@
 //===========10/28=========
 // this is modified by remote
 // create a new branch : bugFix
+
 // ========= 11/23========
 // 随便写点儿
 // lalallalalalal
 // dgategtrwyrshdgtfgsretsfzd
 // 2024-01-03
 // 今天来测试一下 如何撤销提交记录
+
+// this is added by remote on bugFix分支
+// ======10/30==========
+//本地所做的修改：b.js which is on the feature branch on local
+// pull - > push，则会出现两个提交节点：本地的提交；远程代码与本地修改合并过程中自动生成的merge commit节点
+// afodhtaprfadtkerqahfvdszgas: this line is added on feature branch on local
+
+// 2024-10-11 dev 分支做出修改
+
