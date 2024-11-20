98efcb3b5775875bd981049790cf7e991bd368d5
fxy <xyfeng0854@qq.com>
2024-11-20 17:27:19 +0800

    1120:本地新分支

--- b.js
+++ b.js
@@ -7,3 +7,4 @@
 //本地所做的修改：b.js which is on the feature branch on local
 // pull - > push，则会出现两个提交节点：本地的提交；远程代码与本地修改合并过程中自动生成的merge commit节点
 // afodhtaprfadtkerqahfvdszgas: this line is added on feature branch on local
+// 2024-11-20本地新分支push到远程
