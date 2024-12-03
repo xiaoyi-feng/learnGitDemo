184797c1015509702124fab450c3e5ccd985dc56
fxy <xyfeng0854@qq.com>
2024-12-03 19:20:25 +0800

    test to push a new loacl branch to git

--- b.js
+++ b.js
@@ -7,6 +7,4 @@
 //本地所做的修改：b.js which is on the feature branch on local
 // pull - > push，则会出现两个提交节点：本地的提交；远程代码与本地修改合并过程中自动生成的merge commit节点
 // afodhtaprfadtkerqahfvdszgas: this line is added on feature branch on local
-<<<<<<< HEAD
 // 2024-11-20本地新分支push到远程
->>>>>>> deedb0433930a3afa4bc61a4ba8ef245e0242b94
